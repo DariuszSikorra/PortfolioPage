a578b4a3d23160f2726f0d19762ca7a6bb14eb47
Darek <48442595+DariuszSikorra@users.noreply.github.com>
2019-04-29 11:37:38 +0200

    Test React App
    Dodaję apkę pogodową z React do testów

---
+++ weather-app/precache-manifest.2a009748ac71e0789db554e40834602c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d37828a74640a7e5c463277666cf2ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d4220809f65a8837e23",
+    "url": "/static/css/main.8018eaeb.chunk.css"
+  },
+  {
+    "revision": "ffaae2ae622dd97bcfdf",
+    "url": "/static/js/2.4f445564.chunk.js"
+  },
+  {
+    "revision": "2d4220809f65a8837e23",
+    "url": "/static/js/main.40067dde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f2239fcc41cc4345e0be2e9f8729858",
+    "url": "/static/media/background.4f2239fc.jpg"
+  }
+]);
